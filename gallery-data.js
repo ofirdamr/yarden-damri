2270fba9a6ecac3f9f6c109d354171961c046423
ofirdamr <ofirdamr@gmail.com>
2026-05-07 22:02:25 +0300

    Create gallery-data.js

---
+++ gallery-data.js
@@ -0,0 +1,2 @@
+// Auto-generated gallery data
+const GALLERY_IMAGES = [{"u":"https://res.cloudinary.com/dfjwxc1cw/image/upload/v1778134461/yarden_makeup/yarden_makeup_18113142562769799.jpg","a":"אם את מחפשת איפור כלתי שיישאר מושלם כל היום – זה בדיוק זה.\nעמידות, דיוק, וגימור טבעי-יוקרתי שלא יוצא מהאופנה.\n\nהתאריכים לעונת החתונות מתמלאים מהר 🤍\n📩 שרייני את שלך עכשיו\n\n@korenmalihe @galifergan @i"}];
